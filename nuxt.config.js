00234da76d85b8d44718622f2ba37a1be04066df
riccardo venturini <riccardoventurini@riccardos-mbp.home>
2018-09-25 04:38:58 +0100

    pwa and gitignore added

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,7 +42,9 @@ module.exports = {
     // Doc: https://github.com/nuxt-community/axios-module#usage
     '@nuxtjs/axios',
     // Doc: https://bootstrap-vue.js.org/docs/
-    'bootstrap-vue/nuxt'
+    'bootstrap-vue/nuxt',
+    // Doc: https://pwa.nuxtjs.org/setup
+    '@nuxtjs/pwa',
   ],
   /*
   ** Axios module configuration
@@ -59,7 +61,7 @@ module.exports = {
     ** You can extend webpack config here
     */
     extend(config, ctx) {
-      
+
     }
   }
 }
